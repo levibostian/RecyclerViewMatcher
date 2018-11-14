80ac7e7d5321353971ddd95327cc87c52a394db4
Levi Bostian <levi.bostian@gmail.com>
2018-11-14 11:56:35 -0600

    Create project. Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Release](https://jitpack.io/v/levibostian/RecyclerViewMatcher.svg)](https://jitpack.io/#levibostian/RecyclerViewMatcher)
+
+# RecyclerViewMatcher
+
+
+
+# Credits
+
+Thank you very much to StackOverflow user [akshay-shahane](https://stackoverflow.com/users/7372771/akshay-shahane) for the inspiration of the project. I had an idea of what I wanted to accomplish but was not sure how to accomplish it. Thanks to [this StackOverflow answer](https://stackoverflow.com/a/45574174/1486374), I had the inspiration and starting code to build this project. Thank you!
---
+++ recyclerviewmatcher/src/main/java/com/levibostian/recyclerviewmatcher/RecyclerViewMatcher.kt
@@ -0,0 +1,93 @@
+package com.levibostian.recyclerviewmatcher
+
+import android.content.res.Resources
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import androidx.annotation.IdRes
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+/**
+ * Object used to obtain a [Matcher] for a [RecyclerView.ViewHolder.itemView] or a specific [View] inside of a [RecyclerView.ViewHolder.itemView].
+ *
+ * Example code to click on a button in the first row of the [RecyclerView]:
+ * ```
+ * onView(RecyclerViewMatcher.recyclerViewWithId(R.id.recyclerview).viewHolderViewAtPosition(0, R.id.adapter_button))
+ *   .perform(click())
+ * ```
+ */
+class RecyclerViewMatcher(@IdRes private val recyclerViewId: Int) {
+
+    companion object {
+        private const val INVALID_ID = -1
+
+        /**
+         * Receive a [RecyclerViewMatcher] with the View ID of the [RecyclerView] you want to interact with.
+         */
+        fun recyclerViewWithId(@IdRes viewId: Int): RecyclerViewMatcher {
+            return RecyclerViewMatcher(viewId)
+        }
+    }
+
+    /**
+     * Receive a [Matcher] for the [RecyclerView.ViewHolder.itemView] at a certain position.
+     *
+     * @param index Zero based index for the row in the [RecyclerView] you want.
+     *
+     * @return [Matcher] you can run assertions and perform actions on.
+     */
+    fun itemViewAtIndex(index: Int): Matcher<View> {
+        return viewHolderViewAtPosition(index, INVALID_ID)
+    }
+
+    /**
+     * Receive a [Matcher] for a specific view inside of a [RecyclerView.ViewHolder] at a certain position.
+     *
+     * @param index Zero based index for the row in the [RecyclerView] you want.
+     * @param targetViewId View id in the [RecyclerView.ViewHolder] you want to interact with.
+     *
+     * @return [Matcher] you can run assertions and perform actions on.
+     */
+    fun viewHolderViewAtPosition(index: Int, @IdRes targetViewId: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            var resources: Resources? = null
+            var itemView: View? = null
+
+            override fun describeTo(description: Description) {
+                var idDescription = recyclerViewId.toString()
+
+                resources?.let { resources ->
+                    idDescription = try {
+                        resources.getResourceName(recyclerViewId)
+                    } catch (notFound: Resources.NotFoundException) {
+                        "%s (resource name not found)".format(Integer.valueOf(recyclerViewId))
+                    }
+                }
+
+                description.appendText("RecyclerView view with id: $idDescription not found.")
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+                this.resources = view.resources
+
+                if (itemView == null) {
+                    val recyclerView: RecyclerView? = view.rootView.findViewById(recyclerViewId) as? RecyclerView
+                    if (recyclerView != null && recyclerView.id == recyclerViewId) {
+                        itemView = recyclerView.findViewHolderForAdapterPosition(index)!!.itemView
+                    } else {
+                        return false
+                    }
+                }
+
+                return if (targetViewId == INVALID_ID) {
+                    view === itemView
+                } else {
+                    val targetView = itemView!!.findViewById<View>(targetViewId)
+                    view === targetView
+                }
+
+            }
+        }
+    }
+}
