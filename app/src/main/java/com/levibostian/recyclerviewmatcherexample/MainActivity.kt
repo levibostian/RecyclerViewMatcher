42c565d4b9665e66717368fa9d2afc7e2802d728
Levi Bostian <levi@levi.earth>
2021-04-29 12:26:44 -0500

    chore: update dependencies

--- app/src/main/java/com/levibostian/recyclerviewmatcherexample/MainActivity.kt
+++ app/src/main/java/com/levibostian/recyclerviewmatcherexample/MainActivity.kt
@@ -3,14 +3,17 @@ package com.levibostian.recyclerviewmatcherexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import com.levibostian.recyclerviewmatcherexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onStart() {
@@ -22,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             "battlestar galactica"
         )
 
-        act_main_recyclerview.apply {
+        binding.actMainRecyclerview.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = MainRecyclerViewAdapter(data)
         }
